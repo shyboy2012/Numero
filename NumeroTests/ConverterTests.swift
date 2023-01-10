afd8008542b1ddbde4298b9151bd0264163326dd
shyboy2012 <994741327@qq.com>
2023-01-10 11:04:50 +0800

    test trigger jenkins job automatically

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -46,7 +46,7 @@ class ConverterTests: XCTestCase {
   
   func testConversionForOne() {
     let result = converter.convert(1)
-    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    XCTAssertEqual(result, "", "Conversion for 1 is incorrect")
   }
   
   func testConversionForTwo() {
